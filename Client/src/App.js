4f2fbfda60361f1acc242566c9483be850a18a56
mehdialamiidrissi <M.alamiidrissi@aui.ma>
2022-07-30 00:40:52 +0100

    fetching music from spotify

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,13 @@
 import Home from './Components/Home';
 import Graph from './Components/Graph';
 import {Routes, Route} from 'react-router-dom';
-var data = require("./Components/MOCK_DATA_SLIDER.json");
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/search/:id" element={<Graph slides={data}/>}/>
+      <Route path="/search/:id" element={<Graph/>}/>
     </Routes>
     
     
